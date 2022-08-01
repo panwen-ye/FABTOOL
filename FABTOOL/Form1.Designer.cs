94dd76129792a41fab4e3b92e11ee726b63fe194
jojo <1650736419@qq.com>
2022-08-01 21:13:00 +0800

    添加项目文件。

---
+++ FABTOOL/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FABTOOL
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始任务";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textName
+            // 
+            this.textName.AutoSize = true;
+            this.textName.Location = new System.Drawing.Point(71, 21);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(56, 17);
+            this.textName.TabIndex = 1;
+            this.textName.Text = "姓      名";
+            this.textName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(133, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "工      号";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(133, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(133, 76);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(133, 23);
+            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateStart_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "开始时间";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 332);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "FAB TOOL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label textName;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private DateTimePicker dateTimePicker1;
+        private Label label3;
+    }
+}
---
+++ FABTOOL/Form1.cs
@@ -0,0 +1,205 @@
+using Newtonsoft.Json;
+using System.Net;
+
+namespace FABTOOL
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.MaxDate = DateTime.Now;
+        }
+
+        public class FabInfo {
+            public string? CoputerName;
+            public string? UserId;
+            public string? TaskTime;
+
+            public List<FileTimeInfo>? Infos;
+        }
+
+        //自定义一个类
+        public class FileTimeInfo
+        {
+            public string? FileName;  //文件名3
+            public string? DirectoryName;
+
+            public DateTime FileCreateTime; //创建时间
+            public DateTime LastWriteTime; //创建时间
+            public DateTime LastAccessTime; //创建时间
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String name = textBox1.Text;
+            String userId = textBox2.Text;
+            String startStr = dateTimePicker1.Text;
+            if (string.IsNullOrEmpty(name) ) {
+                MessageBox.Show("name can't be empty !");
+            }
+            if (string.IsNullOrEmpty(userId))
+            {
+                MessageBox.Show("empId can't be empty !");
+            }
+            if (string.IsNullOrEmpty(startStr))
+            {
+                MessageBox.Show("startStr can't be empty !");
+            }
+
+
+
+
+            FabInfo fabInfo = new FabInfo();
+
+            fabInfo.CoputerName = Dns.GetHostName();
+            fabInfo.TaskTime = startStr;
+            fabInfo.UserId = userId;
+
+
+
+            List<String> dirvers = getDirvers();
+            
+            string[] exts = new string[] { };
+            /*string[] exts = new string[] {".TXT" , ".DOC" , ".EXE" };*/
+
+            List<FileTimeInfo> list1 = new List<FileTimeInfo>();
+
+            
+            foreach (String dir in dirvers) {
+                List< FileTimeInfo> a =  GetLatestFileTimeInfo(dir, exts);
+                list1.AddRange(a);
+            };
+
+            List<FileTimeInfo> list2 = new List<FileTimeInfo>();
+            foreach (FileTimeInfo fileTimeInfo in list1)
+            {
+                DateTime dt1 = Convert.ToDateTime(startStr);
+                // file last modify time > start time
+                if (DateTime.Compare(dt1, fileTimeInfo.LastWriteTime) < 0)
+                    list2.Add(fileTimeInfo);
+            }
+            fabInfo.Infos = list2;
+
+
+            string jsondata = JsonConvert.SerializeObject(fabInfo);  //class类转string
+            using (StreamWriter sw = new StreamWriter(@"FabTool.json"))  //将string 写入json文件
+            {
+                sw.WriteLine(jsondata);
+            }
+
+            using StreamWriter file = new(@"FabTool1.txt", false);
+            file.WriteLine("name : " + fabInfo.UserId);
+            file.WriteLine("computername : " + fabInfo.CoputerName);
+            file.WriteLine("taskTime : " + fabInfo.TaskTime);
+            foreach (FileTimeInfo line in fabInfo.Infos)
+            {
+
+                file.WriteLine(JsonConvert.SerializeObject(line));
+            }
+
+
+            MessageBox.Show("file write over");
+        }
+
+        
+
+        private List<String> getDirvers() {
+            List<String> list = new List<string>();
+            var drivers = DriveInfo.GetDrives();
+            foreach (var driver in drivers)
+            {
+                list.Add(driver.Name);
+            }
+
+            return list;
+
+        }
+
+
+       
+
+        //获取最近创建的文件名和创建时间
+        //如果没有指定类型的文件，返回null
+        static List<FileTimeInfo> GetLatestFileTimeInfo(string dir, string[] exts)
+        {
+            List<FileTimeInfo> list = new List<FileTimeInfo>();
+            DirectoryInfo root = new DirectoryInfo(dir);
+
+            foreach (FileInfo fi in root.GetFiles())
+            {
+                if (exts.Length != 0)
+                {
+                    if (exts.Contains(fi.Extension.ToUpper()))
+                    {
+                        list.Add(new FileTimeInfo()
+                        {
+                            FileName = fi.FullName,
+                            FileCreateTime = fi.CreationTime,
+                            LastWriteTime = fi.LastWriteTime,
+                            LastAccessTime = fi.LastAccessTime
+                        });
+
+                    }
+
+                }
+                else {
+                    list.Add(new FileTimeInfo()
+                    {
+                        FileName = fi.FullName,
+                        DirectoryName =fi.DirectoryName,
+                        FileCreateTime = fi.CreationTime,
+                        LastWriteTime = fi.LastWriteTime,
+                        LastAccessTime = fi.LastAccessTime
+                    });
+
+
+                }
+
+
+                 
+            }
+   
+            return list;
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateStart_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimeChoser1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+         
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
