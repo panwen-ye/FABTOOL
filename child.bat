a0454398cefb3ed926212f5b0a67feae2a00f084
jojo_ye <69068291+panwen-ye@users.noreply.github.com>
2023-03-21 23:17:23 +0800

    Add files via upload

--- child.bat
+++ child.bat
@@ -1,6 +1,7 @@
 @echo off
-chcp 65001 > nul
+
 setlocal enabledelayedexpansion
+
 set drive=%1
 set start_datetime=%2
 set end_datetime=%3
@@ -10,11 +11,10 @@ set file_type=%6
 
 set "output=%drive:~0,1%_temp.txt"
 
-echo %date% %time% 开始扫描盘符 %drive% >> %output%
+echo %date% %time% start scan  %drive% >> %output%
 echo %start_datetime% 
 echo %end_datetime%
 echo "%drive%"
-:: 修改比较方式，使用 IF NOT 和 GTR/LSS 操作符
 
 for /f "usebackq delims=" %%d in (`dir /s /b /a-d 	
 	"%drive%\*.doc" "%drive%\*.docx" "%drive%\*.xls" "%drive%\*.xlsx" "%drive%\*.ppt" "%drive%\*.pptx" "%drive%\*.pdf" "%drive%\*.jpg" "%drive%\*.jpeg" "%drive%\*.png" "%drive%\*.bmp"`
@@ -28,6 +28,6 @@ for /f "usebackq delims=" %%d in (`dir /s /b /a-d
 	)
 )
 
-echo %date% %time% 扫描盘符 %drive% 完成 >> %output%
+echo %date% %time% scan  %drive% finish >> %output%
 
 
--- main.bat
+++ main.bat
@@ -1,7 +1,12 @@
 @echo off
-chcp 65001 > nul
-
 setlocal enabledelayedexpansion
+echo "%CD:~0,2%"
+echo "%SystemDrive%"
+if not "%CD:~0,2%"=="%SystemDrive%" (
+    echo "Please execute the script on a local drive."
+    exit /B
+)
+
 
 set "start_datetime=20230115000000"
 set "end_datetime=20230318235959"
@@ -9,29 +14,50 @@ set "end_datetime=20230318235959"
 set "excluded_folders=C:\Windows;C:\Program Files;C:\Program Files (x86)"
 set "excluded_drives=C:"
 
-echo "正在扫描，请稍候..."
-echo "开始时间：%start_datetime%"
-echo "结束时间：%end_datetime%"
-echo "免扫描目录：%excluded_folders%"
-echo "免扫描盘符：%excluded_drives%"
+echo "scan start , wait..."
+echo "start date: %start_datetime%"
+echo "end date: %end_datetime%"
+rem 免扫描目录
+echo "skip folders:%excluded_folders%"
+rem 免扫描盘
+echo "skip drivers:%excluded_drives%"
 
 set result=pass
+
+REM 获取系统语言
+for /f "tokens=3 delims=: " %%a in ('reg query "HKCU\Control Panel\International" /v "sLanguage"') do set lang=%%a
+
+if %lang%==409 (
+    REM 英文格式时间
+	echo "en"
+    set "timestamp=%date:~10,4%%date:~4,2%%date:~7,2%%time:~0,2%%time:~3,2%%time:~6,2%"
+) else (
+    REM 中文格式时间
+	echo "ch"
+    set "timestamp=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
+	echo %date:~0,4%
+	echo %date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%
+)
+
+
 set "dir_name=%COMPUTERNAME%_!date:~5,2!!date:~8,2!!time:~0,2!!time:~3,2!!time:~6,2!"
 echo "!dir_name!"
 
-echo "正在获取本地盘符..."
+echo "get local drivers..."
 for /f "skip=1 tokens=1,2" %%a in ('wmic logicaldisk get deviceid^,drivetype') do (
     if "%%b"=="3" (
         set "flag=Y"
         for %%c in (%excluded_drives%) do (
             if /i "%%a"=="%%c" (
-                echo "跳过盘符 %%a"
+                echo "skip %%a"
                 set "flag=N"
             )
         )
         if "!flag!"=="Y" (
-            echo "启用新进程扫描盘符 %%a"
-			start /b cmd /c child.bat %%a !start_datetime! !end_datetime! "!excluded_folders!" !dir_name!
+            echo "Enables a scan of new processes : %%a"			
+			
+			start "child_%%a.bat"  cmd /c child.bat  %%a !start_datetime! !end_datetime! "!excluded_folders!" !dir_name! 
+			rem start /b cmd /c child.bat %%a !start_datetime! !end_datetime! "!excluded_folders!" !dir_name!
 			echo %%a "!start_datetime!" "!end_datetime!" "!excluded_folders!" "!excluded_drives!"  !dir_name!
         )
     )
@@ -39,28 +65,41 @@ for /f "skip=1 tokens=1,2" %%a in ('wmic logicaldisk get deviceid^,drivetype') d
 
 :check_process
 set "process_count=0"
-for /f "skip=3 tokens=1" %%a in ('tasklist /fi "imagename eq cmd.exe" /fo csv') do (
-    set /a "process_count+=1"
+
+
+for /f "skip=1 tokens=1,2" %%a in ('wmic logicaldisk get deviceid^,drivetype') do (
+	if "%%b"=="3" (
+		set "window_title=child_%%a.bat"
+		set "window_title=!window_title:'=""!"
+		for /f "skip=1 tokens=1" %%c in ('tasklist /fi "imagename eq cmd.exe" /fi "windowtitle eq !window_title!" /v /fo csv') do (
+			echo %%c
+			set /a "process_count+=1"
+		)
+	)
 )
-if %process_count% GTR 1 (
+
+echo Total number of child command prompt processes running: %process_count%
+
+
+if %process_count% GTR 0 (
     timeout /t 30 /nobreak > nul
-	echo "扫描执行中"
+	echo "scan task is running"
     goto check_process
 )
 
-echo "所有进程已经执行完成"
-echo "开始读取脚本2的输出结果并写入 result.txt"
+echo "all scan process finish"
+echo "Start reading the output of child.bat and write into result.txt"
 
 
-
+rem 创建结果存放目录
 md "!dir_name!"
-
+rem 扫描结果写入txt
 (for %%f in (*.txt) do (
-    echo "盘符：%%~nf"
+    echo "driver:%%~nf"
     type "%%~f"
     echo.
 )) > "!dir_name!\result.txt"
 
-echo "写入 result.txt 完成"
-echo "删除临时文件..."
+echo " result.txt finish"
+echo "delete tmp txt..."
 del *_temp.txt
